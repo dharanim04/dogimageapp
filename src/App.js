9455923a7a243d5be21057c784a73887ef17a874
dharanim04 <dharani.m04@gmail.com>
2021-06-30 22:04:22 +1000

    initial commit for deploying page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Dog } from './Dog';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Dog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dog.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export class Dog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animal: "dog",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    const newAnimal = this.state.animal === "dog" ? "cat" : "dog";
+    console.log(newAnimal);
+    this.setState({ animal: newAnimal });
+  }
+
+  render() {
+    return (
+      <div>
+        <img
+          alt="dog"
+          src={
+            this.state.animal === "dog"
+              ? "https://www.dogfoodadvisor.com/wp-content/uploads/2019/08/golden-retriever-puppy-343x335.jpg"
+              : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Cat_Janna.jpg/150px-Cat_Janna.jpg"
+          }
+        ></img>
+        <button onClick={this.handleClick}>
+          I'm more of a {this.state.animal === "dog" ? "cat" : "dog"} person!
+        </button>
+      </div>
+    );
+  }
+}
